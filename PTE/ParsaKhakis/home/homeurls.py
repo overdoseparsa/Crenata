28835a55ccf7b415029c30c766f65729e60fa888
overdoseparsa <parsakhakiy@gmail.com>
2024-07-20 09:08:12 -0400

    cnnect templates home

--- PTE/ParsaKhakis/home/homeurls.py
+++ PTE/ParsaKhakis/home/homeurls.py
@@ -1,7 +1,8 @@
 from django.urls import path , include
 from django.urls.resolvers import URLPattern
 from django.shortcuts import  HttpResponse
+from .views import start_page
 # this is connectd to the templates and usring somthins
 urlpatterns = [
-    path('' , lambda req : HttpResponse("this page not hellow ".title())  , name='home page ')
+    path('' , start_page  , name='home page')
 ]
--- PTE/ParsaKhakis/home/views.py
+++ PTE/ParsaKhakis/home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
+from django.template import loader 
 
-# Create your views here.
+def start_page(request):
+    data_context = {}
+    temp = loader.get_template('homepage.html')
+    return HttpResponse(
+        temp.render(context=data_context)
+    )
